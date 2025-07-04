89e33b0dcf7824fbc648b2e374faeabdb2d47a55
Mats Løvstrand Berntsen <matslovstrandberntsen@gmail.com>
2025-07-04 18:02:12 +0200

    text

--- AppV2/vitneboksen/src/pages/AdminDashboard.tsx
+++ AppV2/vitneboksen/src/pages/AdminDashboard.tsx
@@ -89,7 +89,7 @@ export default function AdminDashboard() {
               {vb.videosToBeProcessed > 0 &&
               <p className="text-m text-muted"><span className='bg-white text-black text-center min-w-9 inline-block pl-2 pr-2 pt-1 pb-1 rounded'><SpinnerIcon />{vb.videosToBeProcessed}</span> vitnesbyrd behandlingskø </p>
               }
-              <p className="text-m text-muted"><span className='bg-white text-black text-center min-w-9 inline-block pl-2 pr-2 pt-1 pb-1 rounded'>{vb.completedVideos}</span> ferdig {vb.completedVideos > 1 ? "behandlede vitnesbyrd" : "behandlet vitnesbyrd"}</p>
+              <p className="text-m text-muted"><span className='bg-white text-black text-center min-w-9 inline-block pl-2 pr-2 pt-1 pb-1 rounded'>{vb.completedVideos}</span> {vb.completedVideos > 1 ? "behandlede vitnesbyrd" : "behandlet vitnesbyrd"}</p>
             </div>
             </div>
             {Object.values(vb.questions).length > 0 ?
@@ -125,7 +125,7 @@ export default function AdminDashboard() {
                   Rediger
                 </Link>
 
-                {vb.completedVideos > 0 && vb.finalVideoProcessingStatus == FinalVideoStatus.notStarted && 
+                {vb.completedVideos > 1 && vb.finalVideoProcessingStatus == FinalVideoStatus.notStarted && 
                 <button 
                 onClick={() => startFinalVideoProcessing(vb.id)}
                 className=" flex gap-2 bg-primary-button  text-black px-4 py-2 rounded ">
