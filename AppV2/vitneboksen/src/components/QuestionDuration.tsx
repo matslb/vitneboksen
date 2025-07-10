6b4d4e5ca198769217c03613b8520c592dc93136
Mats Løvstrand Berntsen <matslovstrandberntsen@gmail.com>
2025-07-10 03:41:59 +0200

    removing 30

--- AppV2/vitneboksen/src/components/QuestionDuration.tsx
+++ AppV2/vitneboksen/src/components/QuestionDuration.tsx
@@ -15,7 +15,6 @@ export default function QuestionDuration({ recordingDuration, setRecordingDurati
             <option value={15}>15 sekunder</option>
             <option value={20}>20 sekunder</option>
             <option value={25}>25 sekunder</option>
-            <option value={30}>30 sekunder</option>
         </select>
 
     );
