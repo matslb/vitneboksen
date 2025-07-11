b97a682cd142ece76be2a47b3167d85016b4fe49
Mats Berntsen <mats.berntsen@vippsmobilepay.com>
2025-07-11 23:44:43 +0200

    upping limit to 70

--- AppV2/vitneboksen/src/pages/Testimony.tsx
+++ AppV2/vitneboksen/src/pages/Testimony.tsx
@@ -120,7 +120,7 @@ export default function TestimonyPage() {
           onClick={handleEnterFullscreen}
         >Fullskjerm</button>
       }
-      {!vitneboks.isOpen || filteredQuestions.length === 0 || vitneboks.finalVideoProcessingStatus == FinalVideoStatus.started || (vitneboks.completedVideos + vitneboks.videosToBeProcessed) >= 50 ?
+      {!vitneboks.isOpen || filteredQuestions.length === 0 || vitneboks.finalVideoProcessingStatus == FinalVideoStatus.started || (vitneboks.completedVideos + vitneboks.videosToBeProcessed) >= 70 ?
         <div className="flex flex-col items-center justify-center flex-1 p-6 text-3xl">
           Kom tilbake senere. Her er det dessverre stengt 😓
         </div>
