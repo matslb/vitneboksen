517e4cce38161dfea2fd7fb02878b33d3d5dc7ba
Mats Løvstrand Berntsen <matslovstrandberntsen@gmail.com>
2025-12-30 17:16:58 +0100

    text

--- AppV2/vitneboksen/src/components/TankYouScreen.tsx
+++ AppV2/vitneboksen/src/components/TankYouScreen.tsx
@@ -26,7 +26,7 @@ export default function ThankYouScreen({ seconds, setWaiting }: ThankYouScreenPr
   return (
     <div className="flex flex-col items-center justify-center flex-1 p-6">
       <h2 className="text-7xl font-bold mb-16">Takk til deg!</h2>
-      <p className="text-5xl my-4 ">Nå kan du vinke inn neste, og bare kul'n</p>
+      <p className="text-5xl my-4 ">Nå kan du bare kul'n</p>
       <p className="text-3xl my-4">Vitneboksen åpner igjen om {countdown} sekunder…</p>
     </div>
   );
